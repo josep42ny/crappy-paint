0764743b11d891cd8525536c051674c5a554c49d
Josep Fortuny <82282886+josep42ny@users.noreply.github.com>
2024-03-07 22:27:59 +0100

    Change button functionality to show a popup

--- script.js
+++ script.js
@@ -1,28 +1,43 @@
 document.addEventListener("click", (event) => {
     switch (event.target.id) {
-        case "gridBtn":
-            buildGrid(document.querySelector("#gridSize").value);
+        case "buildGridBtn":
+            buildGrid(getInput());
+            return;    
         default:
             return;
-    }
-});
+            }
+        });
+        
+        document.addEventListener("mouseover", (event) => {
+            if (event.target.classList.contains("tile")) {
+                event.target.style.backgroundColor = "black";
+            }
+        });
+        
+        buildGrid(10);
+        function buildGrid(gridSize) {
+            console.log(gridSize, typeof Number(gridSize));
+            const grid = document.querySelector("#gridArea");
+            const tileSize = 500/gridSize;
+            grid.textContent = "";
+            for (let i = 0; i < gridSize*gridSize; i++) {
+                const tile = document.createElement("div");
+                tile.classList.add("tile");
+                tile.style.width = tileSize + "px";
+                tile.style.height = tileSize + "px";
+                grid.appendChild(tile);
+            }
+        }
+        
+        function getInput() {
+            const rawInput = Number(prompt("Enter a size between 1-50")); 
+            if(validate(rawInput)) {
+                return rawInput;
+            } else {
+                return getInput();
+            }
+        }
 
-document.addEventListener("mouseover", (event) => {
-    if (event.target.classList.contains("tile")) {
-        event.target.style.backgroundColor = "black";
-    }
-});
-
-buildGrid(10);
-function buildGrid(gridSize) {
-    const grid = document.querySelector("#gridArea");
-    const tileSize = 500/gridSize;
-    grid.textContent = "";
-    for (let i = 0; i < gridSize*gridSize; i++) {
-        const tile = document.createElement("div");
-        tile.classList.add("tile");
-        tile.style.width = tileSize + "px";
-        tile.style.height = tileSize + "px";
-        grid.appendChild(tile);
-    }
-}
+        function validate(raw) {
+            return (raw >= 1 && raw <= 100) ? true : false;
+        }
