b451c6e42d21dfe9eb382718aa523cdedf1d9957
Josep Fortuny <82282886+josep42ny@users.noreply.github.com>
2024-03-07 21:17:13 +0100

    Add necessary js

--- script.js
+++ script.js
@@ -7,11 +7,22 @@ document.addEventListener("click", (event) => {
     }
 });
 
+document.addEventListener("mouseover", (event) => {
+    if (event.target.classList.contains("tile")) {
+        event.target.style.backgroundColor = "black";
+    }
+});
+
+buildGrid(10);
 function buildGrid(gridSize) {
     const grid = document.querySelector("#gridArea");
-    for (let i = 0; i < gridSize; i++) {
+    const tileSize = 500/gridSize;
+    grid.textContent = "";
+    for (let i = 0; i < gridSize*gridSize; i++) {
         const tile = document.createElement("div");
         tile.classList.add("tile");
+        tile.style.width = tileSize + "px";
+        tile.style.height = tileSize + "px";
         grid.appendChild(tile);
     }
 }
